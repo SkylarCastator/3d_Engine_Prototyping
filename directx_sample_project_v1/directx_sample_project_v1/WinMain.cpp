f040cac0e95965f621ed94ff83454097ad4e6452
SkylarCastator <aerialchemist@gmail.com>
2023-01-01 23:03:55 -0700

    Intial test project to execute basic window

---
+++ directx_sample_project_v1/directx_sample_project_v1/WinMain.cpp
@@ -0,0 +1,17 @@
+#include "Window.h"
+
+int __stdcall WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPreviousInstance,
+	LPSTR lpCmdLine,
+	int nCcmdShow
+)
+{
+	Window window(1280, 720, "Basic DirectX Turtoial");
+
+	while (true)
+	{
+		if (const auto msgCode = Window::ProcessMessages())
+			return *msgCode;
+	}
+}
